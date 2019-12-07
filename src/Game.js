9bd264cf2339afa296804c3f91243a77645e7eaa
yacmeno <yacine.seffal@gmail.com>
2019-12-07 15:17:46 -0500

    Prettier formatting

--- src/Game.js
+++ src/Game.js
@@ -1,38 +1,45 @@
 export class Game {
     constructor() {
-        this.rolls = new Array()
+        this.rolls = new Array();
     }
 
     roll(nbPins) {
         if (!Number.isInteger(nbPins) || nbPins < 0 || nbPins > 10) {
-            throw new Error("Invalid number of pins")
+            throw new Error("Invalid number of pins");
         }
 
-        this.rolls.push(nbPins)
+        this.rolls.push(nbPins);
     }
 
     score() {
         let totalScore = 0;
-        let rollIndex = 0
+        let rollIndex = 0;
 
         for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
-            const isSpare = this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10 ? true : false
-            const isStrike = this.rolls[rollIndex] === 10 ? true : false
+            const isSpare =
+                this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10
+                    ? true
+                    : false;
+            const isStrike = this.rolls[rollIndex] === 10 ? true : false;
 
             if (isSpare) {
-                totalScore += this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2]
-                rollIndex += 2
+                totalScore +=
+                    this.rolls[rollIndex] +
+                    this.rolls[rollIndex + 1] +
+                    this.rolls[rollIndex + 2];
+                rollIndex += 2;
             } else if (isStrike) {
-                totalScore += this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2]
-                rollIndex++
+                totalScore +=
+                    this.rolls[rollIndex] +
+                    this.rolls[rollIndex + 1] +
+                    this.rolls[rollIndex + 2];
+                rollIndex++;
             } else {
-                totalScore += this.rolls[rollIndex] + this.rolls[rollIndex + 1]
-                rollIndex += 2
+                totalScore += this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+                rollIndex += 2;
             }
         }
 
-        
         return totalScore;
     }
-
 }
--- test/Game.test.js
+++ test/Game.test.js
@@ -1,64 +1,63 @@
-import { Game } from '../src/Game';
+import { Game } from "../src/Game";
 
-const rollMany = (game, nbRolls, nbPins) => { 
+const rollMany = (game, nbRolls, nbPins) => {
     for (let i = 0; i < nbRolls; i++) {
-          game.roll(nbPins)
+        game.roll(nbPins);
     }
- }
+};
 
-test('Should throw error on invalid pin number', () => {
+test("Should throw error on invalid pin number", () => {
     const game = new Game();
 
     expect(() => {
-        game.roll('octopus');
+        game.roll("octopus");
     }).toThrow();
 });
 
-test('Should handle all 0s', () => {
+test("Should handle all 0s", () => {
     const game = new Game();
 
-    rollMany(game, 20, 0)
+    rollMany(game, 20, 0);
 
-    expect(game.score()).toBe(0)
+    expect(game.score()).toBe(0);
 });
 
-
-test('Should handle all 1s (no spare no strike)', () => {
+test("Should handle all 1s (no spare no strike)", () => {
     const game = new Game();
 
-    rollMany(game, 20, 1)
+    rollMany(game, 20, 1);
 
-    expect(game.score()).toBe(20)
+    expect(game.score()).toBe(20);
 });
 
-test('Should handle spare', () => {
+test("Should handle spare", () => {
     const game = new Game();
-    
+
     // roll spare on first frame
     // roll no spare no strike on second frame
-    game.roll(5)
-    game.roll(5)
     game.roll(5);
-    rollMany(game, 17, 0)
+    game.roll(5);
+    game.roll(5);
+    rollMany(game, 17, 0);
 
-    expect(game.score()).toBe(20)
+    expect(game.score()).toBe(20);
 });
 
-test('Should handle strike', () => {
-    const game = new Game()
+test("Should handle strike", () => {
+    const game = new Game();
 
-    game.roll(10)
-    game.roll(5)
-    game.roll(2)
-    rollMany(game, 17, 0)
+    game.roll(10);
+    game.roll(5);
+    game.roll(2);
+    rollMany(game, 17, 0);
 
-    expect(game.score()).toBe(24)
+    expect(game.score()).toBe(24);
 });
 
-test('Should handle all strikes game', () => {
-    const game = new Game()
+test("Should handle all strikes game", () => {
+    const game = new Game();
 
-    rollMany(game, 13, 10)
+    rollMany(game, 13, 10);
 
-    expect(game.score()).toBe(300)
+    expect(game.score()).toBe(300);
 });
