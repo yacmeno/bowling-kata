4a8d04b8e0ac4ebadd81def4c5123a07e1699b41
yacmeno <yacine.seffal@gmail.com>
2019-12-07 12:56:36 -0500

    Refactor: use array + handling for no strike no spare

--- src/Game.js
+++ src/Game.js
@@ -1,41 +1,6 @@
 export class Game {
     constructor() {
-        // Map of each frame: Map<frameNumber, frameProperties>
-        this.frames = new Map([
-            [0, {
-                rolls: [null, null],
-                score: null
-            }], [1, {
-                rolls: [null, null],
-                score: null
-            }], [2, {
-                rolls: [null, null],
-                score: null
-            }], [3, {
-                rolls: [null, null],
-                score: null
-            }], [4, {
-                rolls: [null, null],
-                score: null
-            }], [5, {
-                rolls: [null, null],
-                score: null
-            }], [6, {
-                rolls: [null, null],
-                score: null
-            }], [7, {
-                rolls: [null, null],
-                score: null
-            }], [8, {
-                rolls: [null, null],
-                score: null
-            }], [9, {
-                rolls: [null, null],
-                score: null
-            }],])
-
-        this.currentFrame = 0;
-        this.currentFrameRoll = 0;
+        this.rolls = new Array()
     }
 
     roll(nbPins) {
@@ -43,56 +8,14 @@ export class Game {
             throw new Error("Invalid number of pins")
         }
 
-        const currentFrameValue = this.frames.get(this.currentFrame);
-        let nextFrameValue = {};
-
-        if (nbPins === 10) {
-            nextFrameValue = {
-                ...currentFrameValue,
-                rolls: [10, null],
-            }
-
-            this.frames.set(this.currentFrame, nextFrameValue)
-            this.currentFrame++
-            this.currentFrameRoll = 0
-        } else {
-            if (this.currentFrameRoll === 0) {
-                nextFrameValue = {
-                    ...currentFrameValue,
-                    rolls: [nbPins, null]
-                }
-
-                this.frames.set(this.currentFrame, nextFrameValue)
-
-                this.currentFrameRoll++
-            } else {
-                nextFrameValue = {
-                    ...currentFrameValue,
-                    rolls: [currentFrameValue.rolls[0], nbPins]
-                }
-
-                this.frames.set(this.currentFrame, nextFrameValue)
-
-                this.currentFrame++
-                this.currentFrameRoll = 0
-            }
-        }
+        this.rolls.push(nbPins)
     }
 
     score() {
-        return;
-    }
+        let totalScore = 0
 
-    debug() {
-        this.roll(4)
-        this.roll(4)
-        this.roll(6)
-        this.roll(4)
-        this.roll(8)
-        this.roll(8)
-
-        console.log(this.frames)
-        // console.log(this.currentFrame);
-
+        this.rolls.forEach(pins => totalScore += pins)
+        
+        return totalScore;
     }
 }
--- test/Game.test.js
+++ test/Game.test.js
@@ -1,38 +1,48 @@
 import { Game } from '../src/Game';
 
-beforeAll(() => {
-    return
-});
-afterAll(() => {
-    return
-});
+const rollMany = (game, nbRolls, nbPins) => { 
+    for (let i = 0; i < nbRolls; i++) {
+          game.roll(nbPins)
+    }
+ }
 
 test('Should throw error on invalid pin number', () => {
     const game = new Game();
+
     expect(() => {
         game.roll('octopus');
     }).toThrow();
 });
-test('Should handle last roll spare', () => {
 
-});
-test('Should handle last roll strike', () => {
-
-});
 test('Should handle all 0s', () => {
-
-});
-test('Should handle all strikes game', () => {
-
-});
-test('Should handle all spare game', () => {
-
-});
-test('Should handle game with normal frames, spares and strikes', () => {
-
-});
-
-test('debug', () => {
     const game = new Game();
-    game.debug()
+
+    rollMany(game, 20, 0)
+
+    expect(game.score()).toBe(0)
 });
+
+
+test('Should handle all 1s (no spare no strike)', () => {
+    const game = new Game();
+
+    rollMany(game, 20, 1)
+
+    expect(game.score()).toBe(20)
+});
+
+// test('Should handle last roll spare', () => {
+
+// });
+// test('Should handle last roll strike', () => {
+
+// });
+// test('Should handle all strikes game', () => {
+
+// });
+// test('Should handle all spare game', () => {
+
+// });
+// test('Should handle game with normal frames, spares and strikes', () => {
+
+// });
